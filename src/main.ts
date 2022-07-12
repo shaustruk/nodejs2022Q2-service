03ab89c36938668997a68151b28b5fe218ecad14
Your Name <you@example.com>
2022-07-12 22:59:22 +0300

    feat: create components

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { AppModule } from './app.module';
 import { dirname, join } from 'path';
 import { parse } from 'yaml';
 import { readFile } from 'fs/promises';
+import 'dotenv/config';
 
+const URL = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const rootDirname = dirname(__dirname);
   const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
   const document = parse(DOC_API);
   SwaggerModule.setup('doc', app, document);
-  await app.listen(3000);
+  await app.listen(URL);
 }
 bootstrap();
