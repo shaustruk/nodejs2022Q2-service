1b1e9ecb9096e1b6929914c550d686fe5ad84f7f
Your Name <you@example.com>
2022-07-12 13:49:46 +0300

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
+  await app.listen(3000);
 }
 bootstrap();
